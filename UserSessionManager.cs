e7c9c8aa5190fef10870f04549352827e56a46f1
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-11-09 21:59:52 -0600

    created user session manager class

---
+++ UserSessionManager.cs
@@ -0,0 +1,42 @@
+﻿namespace SingletonExample
+{
+    public class UserSessionManager
+    {
+        private static UserSessionManager _instance;
+
+        private UserSessionManager()
+        {
+            IsLoggedIn = false;
+            Username = string.Empty;
+            Role = string.Empty;
+        }
+
+        public static UserSessionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new UserSessionManager();
+                return _instance;
+            }
+        }
+
+        public string Username { get; private set; }
+        public string Role { get; private set; }
+        public bool IsLoggedIn { get; private set; }
+
+        public void Login(string username, string role)
+        {
+            Username = username;
+            Role = role;
+            IsLoggedIn = true;
+        }
+
+        public void Logout()
+        {
+            Username = string.Empty;
+            Role = string.Empty;
+            IsLoggedIn = false;
+        }
+    }
+}
